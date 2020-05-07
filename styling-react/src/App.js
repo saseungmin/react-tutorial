a5b6df5a019524eb823f8d6ac72b5e5cfced9f5c
saseungmin <dbd02169@naver.com>
2020-05-07 12:21:04 +0900

    style-react-tutorial

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
     <div>
+      <StyledComponent/>
       <CSSModule/>
     </div>
   );
