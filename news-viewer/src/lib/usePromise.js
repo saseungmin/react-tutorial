fd8b1b9771a5240efd453c2ff68ff815118a091c
saseungmin <dbd02169@naver.com>
2020-05-12 13:44:46 +0900

    usePromise 수정

--- news-viewer/src/lib/usePromise.js
+++ news-viewer/src/lib/usePromise.js
@@ -2,14 +2,13 @@ import { useState, useEffect } from "react";
 
 
 // usePromise의 의존 배열 deps를 파라미터로 받아온다.
-// deps는 useEffect의 의존 배열로 설정
+// deps는(선택된 카테고리 값이 넘어온다.) useEffect의 의존 배열로 설정
+// promiseCreator는 return한 값
 export default function usePromise(promiseCreator, deps){
     // 대기 중/완료/실패에 대한 상태 관리
     const [loading, setLoading] = useState(false);
     const [resolved, setResolved] = useState(null);
     const [error,setError] = useState(null);
-    console.log(deps);
-    console.log(promiseCreator);
     useEffect(()=>{
         const process = async () => {
             setLoading(true);
