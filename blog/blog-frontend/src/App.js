9a3af99a2e65e45976f7bb9f5e8eda40c4542ec6
saseungmin <dbd02169@naver.com>
2020-05-25 12:22:39 +0900

    프론트엔드 회원가입 UI create

---
+++ blog/blog-frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import WritePage from './pages/WritePage';
+
+function App() {
+  return (
+    <>
+      {/*path에 배열을 넣으면 한 라우트 컴포넌트에 여러 개의 경로를 설절할 수 있다.*/}
+      <Route component={PostListPage} path={['/@:username','/']} exact/>
+      <Route component={LoginPage} path="/login"/>
+      <Route component={RegisterPage} path="/register"/>
+      <Route component={WritePage} path="/write"/>
+      <Route component={PostPage} path="/@:username/:postId"/>
+    </>
+  );
+}
+
+export default App;
